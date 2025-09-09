3c1d099d2d9fb0201a1a7b9e71e45a2934714841
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-09 10:13:54 +0700

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,12 @@
+public class day2 {
+    public static void main(String[] args) {
+        System.out.print("wahyuni");
+        System.out.println("");
+        System.out.println("wahyuni");
+        
+        String nama = "wahyuni";
+        String alamat = "majene";
+        
+        System.out.printf(" nama saya %s asal %S.",nama,alamat);
+    }
+}
