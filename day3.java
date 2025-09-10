b1bc822a2b97ceb4d9834233025629fb5a933127
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-10 18:06:07 +0700

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,10 @@
+public class day3 {
+    public static void main(String[] args) {
+        System.out.println("Memahami dan menggunakan komentar ");
+        // komentar single line karna hanya bisa di gunakan di 1 baris koden
+        /*
+        *di contoh ke 2 ada komentar multi line 
+        di komentar ini kita bisa pakai untuk catatan lebih panjang
+        */
+    }
+}
