21a26c46803cacfed6d39313adac4fe140b60f04
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-11 18:16:46 +0700

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,10 @@
+public class day4 {
+  public static void main (String[] args) {
+    System.out.println("Halo,\nSelamat datang di pusat perbelanjaan");
+       System.out.println("Barang\t : tas");
+      System.out.println("Harga\t : RP 200.000");
+      System.out.println("Jumlah\t : 2");
+      System.out.println("Struk tersimpan di : D:\\Belanja\\struk");
+      System.out.println("Diskonn\b 20%");
+  }
+}
