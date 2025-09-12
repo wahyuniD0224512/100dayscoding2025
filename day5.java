be5d1f4f23eb49d824053188143dc9a86f38d3eb
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-12 18:24:09 +0700

    Create day5.java

---
+++ day5.java
@@ -0,0 +1,18 @@
+public class day5{
+  
+  poublic static void main(String []args) {
+    
+          byte umur = 19; //Tipe data byte
+          short tinggi = 141; //Tipe data short
+          int gaji = 500000; //Tipe data int
+          long tabungan = 150000000; //Tipe data long 
+    
+    System.out.println("umur : "+umur);
+    System.out.println("Tinggi badan "+tinggi);
+    System.out.println("Gaji bulanan : "+gaji);
+    System.out.println("Tabungan : "+tabungan);
+        }
+}
+  
+    
+          
