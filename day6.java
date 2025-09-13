f49d3ce344dfd216c48718ef8b86b3b5795edab0
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-13 08:22:49 +0700

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,8 @@
+public class day6 {
+  public static void main(String []args){
+    double beratbadan = 37.7;
+    float tinggibadan = 142.4f;
+    System.out.println("berat badan : "+beratbadan);
+    System.out.println("tinggi badan : "+tinggibadan);
+  }
+}
