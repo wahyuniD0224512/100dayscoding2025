873ce246ababe875193efbfde97531ff40570025
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-14 19:41:57 +0700

    Create day7.java

---
+++ day7.java
@@ -0,0 +1,10 @@
+public class day7 {
+  public static void main(String []args){
+    char insial = 'W';
+    boolean aktif = true;
+
+    System.out.println("Insial nama "+ insial);
+    System.out.println("status aktif : "+aktif);
+  }
+}
+  
