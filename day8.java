0cf73cf7355d8394e6a5a05ce4440605d2dc5e13
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-15 18:15:21 +0700

    Create day8.java

---
+++ day8.java
@@ -0,0 +1,9 @@
+public class day8 {
+    public static void main(String[] args) {
+                String nama ="Wahyuni";
+                String hobi = "membaca";
+                
+                System.out.println("Hii, nama saya "+nama);
+                System.out.println("Hobi saya "+hobi);
+    }
+}
