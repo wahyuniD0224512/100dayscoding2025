ff099629a03014fb7192321b9aee56edb7fd77c8
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-16 19:19:43 +0700

    Create day9.java

---
+++ day9.java
@@ -0,0 +1,15 @@
+public class day9 {
+    public static void main(String[] args) {
+        
+    
+         int angka;
+
+        // nilai awal
+        angka = 10;
+        System.out.println("Nilai awal angka: " + angka);
+
+        // 3. Update variabel
+        angka = 20;
+        System.out.println("nilai setelah di update : "+angka);
+    }
+}
