97ae0f4475c29f60c55116399f4aec7a6590dc91
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-17 19:56:13 +0700

    Create day10.java

---
+++ day10.java
@@ -0,0 +1,12 @@
+public class day10 {
+    public static void main(String[] args) {
+        int angka1 = 18;
+        int angka2 = 5;
+        
+        int hasil = angka1 + angka2;
+        System.out.println("angka pertama : "+angka1);
+        System.out.println("angka kedua : "+angka2);
+        System.out.println("hasil penjumlahan "+hasil);
+        
+    }
+}
