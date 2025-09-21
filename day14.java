9e6d64560e1d4cdcd1edd860d4601f55369512ec
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-21 19:45:38 +0700

    Create day14.java

---
+++ day14.java
@@ -0,0 +1,10 @@
+public class day14 {
+    public static void main(String[] args) {
+        String angkaStr = "15";   
+        int angka = Integer.parseInt(angkaStr); 
+
+        System.out.println("hasil : " + angka);
+    }
+}
+
+ 
