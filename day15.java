416a8fa7de10d940a69d22bb92144240f08023c4
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-22 20:05:02 +0700

    Create day15.java

---
+++ day15.java
@@ -0,0 +1,11 @@
+public class day15 {
+    public static void main(String[] args) {
+        int a = 79;
+        int b = 10;
+        
+        int jumlah = a+=b;
+        int kurang = a-=b;
+        System.out.println("Hasil Penjumlahan : "+jumlah);
+        System.out.println("Hasil Pengurangan : "+kurang);
+    }
+}
