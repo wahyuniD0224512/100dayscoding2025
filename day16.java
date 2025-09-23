1277c477ccb5803034ebe27b27c012b8a7c99ae7
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-23 18:40:58 +0700

    Update day16.java

--- day16.java
+++ day16.java
@@ -1,3 +1,4 @@
+
 public class day16 {
     public static void main(String[] args) {
         int a = 15;
@@ -6,7 +7,7 @@ public class day16 {
         int perkalian = a * b;
         int pembagian = a / b;
         
-        System.out.println(a + " * "+ b +" = "+perkalian);
-        System.out.println(a + " / "+ b +" = "+pembagian);
+        System.out.println("Hasil perkalian : "+perkalian);
+        System.out.println("Hasil pembagian : "+pembagian);
     }
 }
