6439c541a31ff58d5bc4e22d9a50d111d4c64ac9
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-24 19:26:31 +0700

    Create day17.java

---
+++ day17.java
@@ -0,0 +1,9 @@
+public class day17 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 3;
+      
+        int sisabagi = a % b;
+        System.out.println("sisa bagi : "+sisabagi);
+    }
+}
