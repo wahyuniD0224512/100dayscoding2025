5998adace015418f13ff3dbca123dd353af92ae5
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-25 21:12:38 +0700

    Create day18.java

---
+++ day18.java
@@ -0,0 +1,18 @@
+
+public class Day18 {
+    public static void main(String[] args) {
+         byte a = 10;     
+        short b = a;       
+        int c = b;        
+        long d = c;       
+        float e = d;     
+        double f = e;      
+
+        System.out.println("byte   : " + a);
+        System.out.println("short  : " + b);
+        System.out.println("int    : " + c);
+        System.out.println("long   : " + d);
+        System.out.println("float  : " + e);
+        System.out.println("double : "+  f);
+    }
+}
