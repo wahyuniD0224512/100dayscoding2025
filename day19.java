9ac4bb681a24185c3b83f43c3a91f1c024ae7bc0
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-26 19:21:42 +0700

    Create day19.java

---
+++ day19.java
@@ -0,0 +1,11 @@
+public class day19 {
+    public static void main(String[] args) {
+        double a = 123.456;
+        int b = (int) a;     
+        byte c = (byte) b;   
+      
+        System.out.println("double : " + a);
+        System.out.println("int    : " + b);
+        System.out.println("byte   : " + c);
+    }
+}
