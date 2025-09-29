6785c19b67ac4864e3e6594f43dfe2e191909f41
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-29 21:04:09 +0700

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,7 @@
+public class Day22 {
+    public static void main(String[] args) {
+        int sisi = 20;
+        int luasPersegi = sisi*sisi;
+        System.out.println("Luas Persegi : "+ luasPersegi);
+    }
+}
