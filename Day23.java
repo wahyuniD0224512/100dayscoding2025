9c4f71068b081bcf36e234ce6ec696844ef8a93b
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-09-30 19:20:06 +0700

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,10 @@
+public class Day23 {
+    public static void main(String[] args) {
+        int panjang = 12;
+        int lebar = 5;
+        int luasPersegi = panjang*lebar;
+        System.out.println("Panjang : "+panjang);
+        System.out.println("Lebar : "+lebar);
+        System.out.println("Luas Persegi Panjang : "+luasPersegi);
+    }   
+}
