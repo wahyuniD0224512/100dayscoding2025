8b5de559c756820e6575cb44dd5138e2aee9d937
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-10-01 19:52:14 +0700

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,11 @@
+public class Day24 {
+    public static void main(String[] args) {
+        double r = 8; 
+        double phi = 3.14;
+      
+        double luas = phi * r * r;
+
+        System.out.println("Jari-jari: " + r);
+        System.out.println("Luas lingkaran: " + luas);
+    }
+}
