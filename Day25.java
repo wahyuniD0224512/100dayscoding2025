ba83cca8bfa9ccde0f5268c3547b285842f02aee
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-10-02 20:12:52 +0700

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,13 @@
+public class Day25 {
+    public static void main(String[] args) {
+      
+        for (int i = 1; i <=10; i++) {
+            System.out.println("Nilai inkremen = " + i);
+        }
+
+        System.out.println("");
+        for (int j = 10; j > 0; j--) {
+            System.out.println("Nilai dekremen = " + j);
+        }
+    }
+}
