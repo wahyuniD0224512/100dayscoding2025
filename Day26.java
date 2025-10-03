5d14a22b73ce0f59d1de08e7a1f683fcbac01e48
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-10-03 19:40:56 +0700

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,27 @@
+public class Day26 {
+    public static void main(String[] args) {
+        int a = 5;
+      
+        int b = 7;
+        b += 4;
+
+        int c = 10;
+        c -= 6;
+
+        int d = 3;
+        d *= 5;
+
+        int e = 20;
+        e /= 4;
+
+        int f = 9;
+        f %= 4;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+    }
+}
