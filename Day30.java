b3bbdc4f44ffeef2cec5931c3e5b36d20310a936
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-10-07 19:51:55 +0700

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,9 @@
+public class Day30 {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 10;
+
+        System.out.println("Apakah a <= b : " + (a <= b));
+        System.out.println("Apakah a >= b : " + (a >= b));
+    }
+}
