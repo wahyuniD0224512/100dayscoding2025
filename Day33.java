06750af46f8647cfaa8e9bb6013d887a32f4cc62
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-10-10 20:21:30 +0700

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,8 @@
+public class Day33 {
+    public static void main(String[] args) {
+        boolean nilai = true;
+
+        System.out.println("Nilai asli  : " + nilai);
+        System.out.println("Nilai not : " + !nilai);
+    }
+}
