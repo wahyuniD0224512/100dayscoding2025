3e3fb18c4f75282ca079e5ad10c10800aa1606d1
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-10-10 20:36:52 +0700

    Create latihan3.java

---
+++ latihan3.java
@@ -0,0 +1,10 @@
+public class latihan10 {
+    public static void main(String[] args) {
+        int angka= 10;
+        
+        for (int i = 1; i <= angka; i++) {
+            int total = angka*i;
+            System.out.println(i+" x "+angka +" = "+total);
+        }
+    }
+}
