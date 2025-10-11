cd4d14623840249dd59c053390ca02c161753061
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-10-11 20:49:26 +0800

    Create Day34.java

---
+++ Day34.java
@@ -0,0 +1,9 @@
+public class Day34 {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 4;
+        int c = 2;
+        boolean hasil = ((a + b) > (b * c)) && ((a - c) != 0);
+        System.out.println(hasil);  
+    }
+}
