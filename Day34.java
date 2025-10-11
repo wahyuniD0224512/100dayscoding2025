fd227d15cda85223749bf2eb4574dda6b361b3fa
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-10-11 21:04:22 +0800

    Update Day34.java

--- Day34.java
+++ Day34.java
@@ -1,9 +1,10 @@
 public class Day34 {
     public static void main(String[] args) {
-        int a = 8;
-        int b = 4;
-        int c = 2;
-        boolean hasil = ((a + b) > (b * c)) && ((a - c) != 0);
+        int a = 19;
+        int b = 5;
+        int c = 8;
+        int d = 4;
+        boolean hasil = ((a + b) > (b * c)) && ((a - c) != 0) || (d < a);
         System.out.println(hasil);  
     }
 }
