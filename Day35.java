368006984ad82e60eae3aa79fdf2ea69bdcf3199
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-10-12 20:08:05 +0700

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,12 @@
+public class Day35 {
+    public static void main(String[] args) {
+        
+          int umur = 18;
+
+        if (umur >= 17) {
+            System.out.println("Kamu sudah dewasa.");
+        } else {
+            System.out.println("Kamu masih anak-anak.");
+        }
+    }
+}
