da7e29bc37b5825d90074ea2743bb53e2945135d
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-10-28 00:52:01 +0700

    Create Day51.java

---
+++ Day51.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Day51 {
+    public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+        System.out.print("Maukkan batas angka : ");
+        int angka = i.nextInt();
+        for (int j = 1; j <= angka; j++) {
+            System.out.println(j);
+            
+        }
+    }
+    
+}
