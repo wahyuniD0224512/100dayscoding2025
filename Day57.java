00cc9e102630b829932c71b71e5dcf497972d651
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-11-03 21:41:09 +0700

    Create Day57.java

---
+++ Day57.java
@@ -0,0 +1,10 @@
+public class Day57 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 5; j++) { 
+                System.out.print(j + " ");
+            }
+            System.out.println(); 
+        }
+    }
+}
