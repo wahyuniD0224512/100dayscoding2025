1d06883b63e113cfe7fd4aff64648454ecff1065
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-11-11 20:40:30 +0700

    Create Day65.java

---
+++ Day65.java
@@ -0,0 +1,17 @@
+package bilangan.ganjil;
+import java.util.Scanner;
+public class fakktorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int s = 1;
+        System.out.print("Masukkan angka : ");
+        int a = sc.nextInt();
+        System.out.println("Angka yang dimasukkan : "+a);
+        for (int i = 1; i <=  a; i++) {
+            s*=i;
+        }
+        System.out.println("Faktorial dari " + a + " adalah : " + s);
+       
+        }
+    }
+
