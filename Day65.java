8a48ed8d3da1542ac6e6f07e2c44298c8305cd26
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-11-11 20:48:11 +0700

    Update Day65.java

--- Day65.java
+++ Day65.java
@@ -1,6 +1,6 @@
 package bilangan.ganjil;
 import java.util.Scanner;
-public class fakktorial {
+public class Day65 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int s = 1;
