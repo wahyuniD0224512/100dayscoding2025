bd62744bf6075b4547ec29f6236d497569c8af9b
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-11-12 21:31:24 +0700

    Create Day66.java

---
+++ Day66.java
@@ -0,0 +1,14 @@
+package Day66;
+import java.util.Scanner;
+public class java {
+    public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+        System.out.print("masukkan batas panjang vertikal : ");
+        int v =i.nextInt();
+        for (int j = 1; j <= v; j++) {
+            System.out.println("*");
+        }
+    }
+}
+
+    
