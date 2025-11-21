bb06363b1fb3d3e1a70c53c5a10240843d334749
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-11-21 20:51:09 +0800

    Create Day75.java

---
+++ Day75.java
@@ -0,0 +1,8 @@
+public class Day75{
+    public static void main(String[]args){
+        String kalimat = "Hii! nama saya wahyuni";
+        System.out.println("panjang : " +kalimat.length());
+        System.out.println("huruf besar : "+kalimat.toUpperCase());
+        System.out.println("huruf kecil : "+kalimat.toLowerCase());
+    }
+}
