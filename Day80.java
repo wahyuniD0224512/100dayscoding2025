241331b301c20c710099b65580fefd365433cc85
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-11-26 20:07:17 +0700

    Create Day80.java

---
+++ Day80.java
@@ -0,0 +1,9 @@
+public class Day80 {
+    public static void main(String[] args) {
+        int [] arr = {4,7,3,9,4};
+        for(int b : arr){
+            System.out.print(b+" ");
+        }
+    }
+    
+}
