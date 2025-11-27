3a3bc2f83db3effd28a5bc54a9473f9b660f8c36
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-11-27 20:11:09 +0800

    Create Day81.java

---
+++ Day81.java
@@ -0,0 +1,10 @@
+public class Day81 {
+    public static void main(String[] args) {
+
+        int[] angka = {10, 20, 30, 40, 50};
+
+        System.out.println("Isi indeks ke-0 : " + angka[0]);
+        System.out.println("Isi indeks ke-2 : " + angka[2]);
+        System.out.println("Isi indeks ke-4 : " + angka[4]);
+    }
+}
