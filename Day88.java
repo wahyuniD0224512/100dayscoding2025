2419a054721f1bed7b24586694d3e6699bbfe2ff
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-12-04 22:17:13 +0800

    Create Day88.java

---
+++ Day88.java
@@ -0,0 +1,9 @@
+public class Day88 {
+    public static void main(String[] args) {
+        int[] angka = {10, 20, 30, 40, 50};
+        
+        for (int a : angka) {
+            System.out.println(a);
+        }
+    }
+}
