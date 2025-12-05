631b74b682d6e1823a95ff12dbdb23c95eec7e00
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-12-05 20:02:10 +0700

    Create Day89.java

---
+++ Day89.java
@@ -0,0 +1,11 @@
+public class Day89 {
+
+    static void sapa() {
+        System.out.println("hii! perkenalkan nama saya Wahyuni ");
+
+    }
+
+    public static void main(String[] args) {
+        sapa();
+    }
+}
