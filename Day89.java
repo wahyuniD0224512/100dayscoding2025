ac9c89bf342d12edd8549710e25c8b6288b2341c
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-12-05 20:20:35 +0700

    Update Day89.java

--- Day89.java
+++ Day89.java
@@ -1,11 +1,13 @@
+
 public class Day89 {
 
-    static void sapa() {
+     void sapa() {
         System.out.println("hii! perkenalkan nama saya Wahyuni ");
 
     }
 
     public static void main(String[] args) {
-        sapa();
+        Day89 a = new Day89();
+        a.sapa();
     }
 }
