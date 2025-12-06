986652ebb4300aff4fd0b03a19421f42c03edaca
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-12-06 18:13:50 +0700

    Create Day90.java

---
+++ Day90.java
@@ -0,0 +1,13 @@
+
+public class Day90 {
+
+    int tambah(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Day90 a = new Day90();
+        System.out.println(a.tambah(2, 8));
+    }
+
+}
