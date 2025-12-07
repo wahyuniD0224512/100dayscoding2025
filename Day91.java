9aecef9ec2e1ef2dc85406d51dc4a7437a43437f
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-12-07 21:00:00 +0700

    Create Day91.java

---
+++ Day91.java
@@ -0,0 +1,12 @@
+
+public class Day91 {
+
+    static int parameter(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(parameter(2, 3));
+    }
+
+}
