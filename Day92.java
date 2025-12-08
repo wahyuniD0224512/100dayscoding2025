1df2bb19cb91cd3bf37ff260f3127616eb5ac4d0
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-12-08 23:14:10 +0800

    Create Day92.java

---
+++ Day92.java
@@ -0,0 +1,13 @@
+public class Day92 {
+    
+
+     int bagi(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+       Day92 a = new Day92();
+        System.out.println(a.bagi(6, 2));
+    }
+
+}
