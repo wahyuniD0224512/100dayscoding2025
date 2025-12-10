6ced34411d058cc1fe8e78886aaf6199650491cc
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-12-10 21:21:28 +0700

    Create Day94.java

---
+++ Day94.java
@@ -0,0 +1,19 @@
+public class Day94 {
+    void data(String nama, int umur, String alamat){
+        System.out.println("Nama   : " + nama);
+        System.out.println("Umur   : " + umur);
+        System.out.println("Alamat : " + alamat);
+    }
+
+    public static void main(String[] args) {
+        Day94 d = new Day94();
+
+       
+        d.data("Ayu", 20, "Majene");
+    }
+}
+
+
+
+    
+
