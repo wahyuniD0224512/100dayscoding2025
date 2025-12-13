db2b0458873fd75226843de1770c06f72ed8bb8a
wahyuniD0224512 <ayuuuuu771@gmail.com>
2025-12-13 17:59:29 +0700

    Create Day97.java

---
+++ Day97.java
@@ -0,0 +1,9 @@
+public class Day97 {
+static int hitungLuas(int sisi){
+    return sisi * sisi;
+}    
+    public static void main(String[] args) {
+        int hasil = hitungLuas(5);
+        System.out.println("luas persegi : "+hasil);
+    }
+}
